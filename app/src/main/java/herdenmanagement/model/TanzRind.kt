ad4eb8de35b43a85fc5706139268ac2611febb43
greiffenberg <steffen.greiffenberg@ba-dresden.de>
2022-08-31 13:13:55 +0200

    Klasse vereinfacht

--- app/src/main/java/herdenmanagement/model/TanzRind.kt
+++ app/src/main/java/herdenmanagement/model/TanzRind.kt
@@ -1,49 +1,10 @@
 package herdenmanagement.model
 
-import herdenmanagement.model.Threading
-import android.app.Activity
-import kotlin.jvm.JvmOverloads
-import herdenmanagement.model.Gras
-import herdenmanagement.view.PositionElementView
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import de.ba.herdenmanagement.R
-import herdenmanagement.model.Kalb
-import android.widget.FrameLayout
-import herdenmanagement.model.Acker
-import android.text.TextPaint
-import android.content.res.TypedArray
-import herdenmanagement.model.Rindvieh
-import android.view.View.MeasureSpec
-import herdenmanagement.view.GrasView
-import herdenmanagement.view.RindviehView
-import herdenmanagement.view.KalbView
-import android.transition.TransitionManager
-import herdenmanagement.model.Rindvieh.StatusTyp
-import herdenmanagement.model.Rindvieh.RichtungsTyp
-import androidx.appcompat.widget.AppCompatImageView
-import herdenmanagement.model.PositionsElement
-import android.graphics.BlurMaskFilter
-import android.graphics.PorterDuff
-import android.content.res.Resources.NotFoundException
-import herdenmanagement.view.AckerView
-import android.view.ViewGroup
-import android.widget.Toast
-import herdenmanagement.model.BeobachtbaresElement
-import androidx.appcompat.app.AppCompatActivity
-import herdenmanagement.HerdenManager
-import android.os.Bundle
-import herdenmanagement.MainActivity
-
 /**
  * Tanzrinder erben alle Eigenschaften der Klasse [Rindvieh]. Sie können sich also
  * genauso auf einem Acker bewegen. Tanzrinder können zusätzlich seitwärts gehen.
- * Dies ist wichtig, zur Beherrschung für die meisten Standardtänze und lateinamerikanische
+ * Dies ist wichtig zur Beherrschung der meisten Standardtänze und lateinamerikanische
  * Bewegungsabläufe.
- *
- *
- * Es wird sichergestellt, dass die Kuh nicht über den Rand des Ackers hinaus gehen kann.
- *
  *
  * Im Muster Model View Controller sind Objekte dieser Klasse Bestandteil des Model.
  *
